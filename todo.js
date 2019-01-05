b0feb6053550159026d0ae1ce73f287effc8c7ae
Hamdi Çatal <hamdicatal@users.noreply.github.com>
2019-01-05 11:50:34 +0300

    Arayüz düzenlendi.

---
+++ todo.js
@@ -0,0 +1,7 @@
+const form = document.querySelector("#todo-form");
+const todoInput = document.querySelector("#todo");
+const todoList = document.querySelector(".list-group");
+const leftCardBody = document.querySelectorAll(".card-body")[0];
+const rightCardBody = document.querySelectorAll(".card-body")[0];
+const filter = document.querySelector("#filter");
+const clearButton = document.querySelector("#clear-all");
