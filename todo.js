957cfacf07a8b06e419207e0ff20d1ef797b75c7
Hamdi Çatal <hamdicatal@users.noreply.github.com>
2019-01-06 12:48:12 +0300

    Taskların arayüzden silinmesi sağlandı

--- todo.js
+++ todo.js
@@ -3,15 +3,24 @@ const form = document.querySelector("#todo-form");
 const todoInput = document.querySelector("#todo");
 const todoList = document.querySelector(".list-group");
 const leftCardBody = document.querySelectorAll(".card-body")[0];
-const rightCardBody = document.querySelectorAll(".card-body")[0];
+const rightCardBody = document.querySelectorAll(".card-body")[1];
 const filter = document.querySelector("#filter");
 const clearButton = document.querySelector("#clear-all");
 
 eventListeners();
 
 function eventListeners(){
-    form.addEventListener("submit", addTodo);
-    document.addEventListener("DOMContentLoaded", loadAllTodosFromStorage);
+    form.addEventListener("submit", addTodo); // for adding new todo
+    document.addEventListener("DOMContentLoaded", loadAllTodosFromStorage); // load all todos to UI
+    rightCardBody.addEventListener("click", deleteTodo); // for deleting todo item
+}
+
+// delete todo from list (and of course from storage)
+function deleteTodo(e){
+    if(e.target.className == "fa fa-remove"){
+        e.target.parentElement.parentElement.remove();
+        showAlert("success", "Task successfully removed!");
+    }
 }
 
 // list all todos when document loaded
